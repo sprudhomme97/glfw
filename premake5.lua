9d1476d4af328475c2355c0e5a712fcbbe06da81
SimonPH <simonprudhomme1997@gmail.com>
2020-07-29 09:44:54 -0400

    Clean up the premake file and update it to match with Maya which is compile as a static library now

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -18,26 +19,29 @@ project "GLFW"
 	}
 
 	filter "system:windows"
-		buildoptions { "-std=c11", "-lgdi32"}
-		staticruntime "On"
 		systemversion "latest"
 	
-	files{
-		"src/win32_init.c",
-		"src/win32_joystick.c",
-		"src/win32_monitor.c",
-		"src/win32_time.c",
-		"src/win32_thread.c",
-		"src/win32_window.c",
-		"src/wgl_context.c",
-		"src/egl_context.c",
-		"src/osmesa_context.c"
-	}
+		files{
+			"src/win32_init.c",
+			"src/win32_joystick.c",
+			"src/win32_monitor.c",
+			"src/win32_time.c",
+			"src/win32_thread.c",
+			"src/win32_window.c",
+			"src/wgl_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c"
+		}
 
-	defines{
-		"_GLFW_WIN32",
-		"_CRT_SECURE_NO_WARNINGS"
-	}
+		defines{
+			"_GLFW_WIN32",
+			"_CRT_SECURE_NO_WARNINGS"
+		}
 
-	filter{ "system:windows", "configurations:Release" }
-		buildoptions "/MT"
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
